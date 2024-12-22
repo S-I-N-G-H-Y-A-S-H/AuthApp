7294c4290b3305ec512e9fee16902b50b5045f23
yash <singhyash4958@gmail.com>
2024-12-22 13:06:01 +0530

    Made urls path and views for form in users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Account Successfully created. Please Log in')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request,'users/signup.html',{'form' : form})    
